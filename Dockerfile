b7a2f256b6476f3d59e07c5d6d224c169dec2916
Yue Zhu <1224535+yuezhu@users.noreply.github.com>
2020-07-30 23:01:59 -0400

    Create rsyncd.conf in entrypoint

--- Dockerfile
+++ Dockerfile
@@ -1,11 +1,14 @@
 FROM alpine:3.10
-RUN apk add --no-cache tzdata shadow rsync
+RUN \
+    apk add --no-cache bash tzdata shadow rsync
 
 RUN \
     sed -i 's/^CREATE_MAIL_SPOOL=yes/CREATE_MAIL_SPOOL=no/' /etc/default/useradd && \
     groupmod -g 100 users && \
     useradd -u 1024 -U -d /app -s /bin/false app && \
     usermod -G users app
+
+VOLUME /backup
 
 ENV TZ=America/New_York
 
--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,6 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e -x
 
 PUID=${PUID:-1024}
 PGID=${PGID:-100}
@@ -6,6 +8,18 @@ PGID=${PGID:-100}
 groupmod -o -g "$PGID" app
 usermod -o -u "$PUID" app
 
-set -e -x
+cat > /etc/rsyncd.conf <<'EOF'
+uid        = app
+gid        = app
+use chroot = yes
+charset    = utf-8
+pid file   = /var/run/rsyncd.pid
+timeout    = 300
 
-exec rsync --no-detach --daemon --config /app/rsyncd.conf
+[backup]
+    path      = /backup
+    comment   = Backup Area
+    read only = false
+EOF
+
+exec rsync --no-detach --daemon
